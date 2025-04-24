1c90b2374ff5a4bc190dea71267b1e98219c3e81
IlVez23 <andreaverba@gmail.com>
2025-04-24 20:59:22 +0200

    attempt app to start hugging face

---
+++ app.py
@@ -0,0 +1,5 @@
+# This file is just a placeholder to satisfy Hugging Face's initial check.
+# Your main application is run by the Dockerfile.
+
+if __name__ == "__main__":
+    print("Hugging Face Space is using Docker!")
